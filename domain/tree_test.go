2a2c4c986de09e183bd6056144913accc7062d84
pploszczyca <piotrus.plo@gmail.com>
2024-05-27 19:17:20 +0200

    Added tests for tree

---
+++ domain/tree_test.go
@@ -0,0 +1,46 @@
+package domain
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNode_AddChild(t *testing.T) {
+	t.Run("adds child to node", func(t *testing.T) {
+		node := Node{}
+		child := &Node{}
+		secondChild := &Node{}
+		expectedChildren := []*Node{child, secondChild}
+
+		node.AddChild(child)
+		node.AddChild(secondChild)
+
+		if !reflect.DeepEqual(node.Children, expectedChildren) {
+			t.Errorf("expected %v but got %v", expectedChildren, node.Children)
+		}
+	})
+}
+
+func TestNode_IsLeaf(t *testing.T) {
+	t.Run("returns false if node doesnt have children and token", func(t *testing.T) {
+		node := Node{}
+		if node.IsLeaf() {
+			t.Errorf("expected false but got true")
+		}
+	})
+
+	t.Run("returns false if node has children", func(t *testing.T) {
+		node := Node{}
+		node.AddChild(&Node{})
+		if node.IsLeaf() {
+			t.Errorf("expected false but got true")
+		}
+	})
+
+	t.Run("returns true if node has token", func(t *testing.T) {
+		node := Node{Token: Token{Type: Identifier, Value: "A"}}
+		if !node.IsLeaf() {
+			t.Errorf("expected true but got false")
+		}
+	})
+}
