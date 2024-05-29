e81f012e83a6536399f4c6b165699b9b97d69691
pploszczyca <piotrus.plo@gmail.com>
2024-05-29 11:19:44 +0200

    Added parsing single expression

--- parser/expression/expressionParser.go
+++ parser/expression/expressionParser.go
@@ -3,6 +3,7 @@ package expression
 import (
 	"GoTinyBasicCompiler/domain"
 	"GoTinyBasicCompiler/parser"
+	"fmt"
 )
 
 type expressionParser struct {
@@ -20,7 +21,57 @@ func (e expressionParser) Parse(tokens []domain.Token, currentIndex int) (*domai
 		currentIndex++
 	}
 
-	// TODO: Add parsing of other expression types
+	newIndex, node, i, err := e.parseFactor(tokens, currentIndex, expressionNode)
+	if err != nil {
+		return node, i, err
+	}
+	currentIndex = newIndex
+
+	for {
+		if tokens[currentIndex].Type == domain.Multiply || tokens[currentIndex].Type == domain.Divide {
+			expressionNode.AddChild(&domain.Node{Token: tokens[currentIndex]})
+			currentIndex++
+
+			newIndex, node, i, err = e.parseFactor(tokens, currentIndex, expressionNode)
+			if err != nil {
+				return node, i, err
+			}
+			currentIndex = newIndex
+			expressionNode.AddChild(&domain.Node{Token: tokens[currentIndex]})
+		} else {
+			newIndex, node, i, err = e.parseFactor(tokens, currentIndex, expressionNode)
+			if err != nil {
+				break
+			}
+			currentIndex = newIndex
+			expressionNode.AddChild(&domain.Node{Token: tokens[currentIndex]})
+		}
+	}
+
+	// TODO: Add parsing list of expressions
 
 	return expressionNode, currentIndex, nil
+}
+
+func (e expressionParser) parseFactor(tokens []domain.Token, currentIndex int, expressionNode *domain.Node) (int, *domain.Node, int, error) {
+	if tokens[currentIndex].Type == domain.Number || tokens[currentIndex].Type == domain.Identifier {
+		expressionNode.AddChild(&domain.Node{Token: tokens[currentIndex]})
+		currentIndex++
+	} else if tokens[currentIndex].Type == domain.LParen {
+		expressionNode.AddChild(&domain.Node{Token: tokens[currentIndex]})
+		currentIndex++
+		expressionNode, newIndex, err := e.Parse(tokens, currentIndex)
+		if err != nil {
+			return 0, nil, newIndex, err
+		}
+		currentIndex = newIndex
+		if tokens[currentIndex].Type != domain.RParen {
+			return 0, nil, currentIndex, fmt.Errorf("expected RParen token, but got %v", tokens[currentIndex].Type)
+		}
+		expressionNode.AddChild(&domain.Node{Token: tokens[currentIndex]})
+		currentIndex++
+	} else {
+		return 0, nil, currentIndex, fmt.Errorf("unexpected token: %v", tokens[currentIndex].Type)
+	}
+	return currentIndex, nil, 0, nil
 }
