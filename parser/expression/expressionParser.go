76f70b572616c160dfe87aeda8629b7ba1f8e124
pploszczyca <piotrus.plo@gmail.com>
2024-05-30 16:23:55 +0200

    Rewrote expression parser

--- parser/expression/expressionParser.go
+++ parser/expression/expressionParser.go
@@ -3,88 +3,45 @@ package expression
 import (
 	"GoTinyBasicCompiler/domain"
 	"GoTinyBasicCompiler/parser"
-	"fmt"
 )
 
 type expressionParser struct {
+	termParser parser.NodeParser
 }
 
-func NewExpressionParser() parser.NodeParser {
-	return &expressionParser{}
+func NewExpressionParser(
+	termParser parser.NodeParser,
+) parser.NodeParser {
+	return &expressionParser{
+		termParser: termParser,
+	}
 }
 
 func (e expressionParser) Parse(iterator *domain.TokenIterator) (*domain.Node, error) {
 	expressionNode := &domain.Node{Type: domain.ExpressionNode}
 
-	token, err := iterator.Current()
-	if err != nil {
-		return nil, err
-	}
-
-	if token.Type == domain.Plus || token.Type == domain.Minus {
-		expressionNode.AddChildToken(token)
-		iterator.Next()
-	}
-
-	err = e.parseFactor(iterator, expressionNode)
-	if err != nil {
-		return nil, err
-	}
-
-	token, err = iterator.Current()
-	if err != nil {
-		return nil, err
-	}
-
 	for {
-		if token.Type == domain.Multiply || token.Type == domain.Divide {
+		token, err := iterator.Current()
+		if err != nil {
+			return nil, err
+		}
+
+		if token.Type == domain.Plus || token.Type == domain.Minus {
 			expressionNode.AddChildToken(token)
 			iterator.Next()
+		}
 
-			err = e.parseFactor(iterator, expressionNode)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			err = e.parseFactor(iterator, expressionNode)
-			if err != nil {
-				break
-			}
+		termNode, err := e.termParser.Parse(iterator)
+		if err != nil {
+			return nil, err
+		}
+		expressionNode.AddChild(termNode)
+
+		token, err = iterator.Current()
+		if err != nil || (token.Type != domain.Plus && token.Type != domain.Minus) {
+			break
 		}
 	}
-
-	// TODO: Add parsing list of expressions
 
 	return expressionNode, nil
-}
-
-func (e expressionParser) parseFactor(iterator *domain.TokenIterator, expressionNode *domain.Node) error {
-	token, err := iterator.Current()
-	if err != nil {
-		return err
-	}
-
-	if token.Type == domain.Number || token.Type == domain.Identifier {
-		expressionNode.AddChildToken(token)
-		iterator.Next()
-	} else if token.Type == domain.LParen {
-		expressionNode.AddChildToken(token)
-		iterator.Next()
-		expressionNode, err := e.Parse(iterator)
-		if err != nil {
-			return err
-		}
-		token, err = iterator.Current()
-		if err != nil {
-			return err
-		}
-		if token.Type != domain.RParen {
-			return fmt.Errorf("expected RParen token, but got %v", token.Type)
-		}
-		expressionNode.AddChildToken(token)
-		iterator.Next()
-	} else {
-		return fmt.Errorf("unexpected token: %v", token.Type)
-	}
-	return nil
 }
