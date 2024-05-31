7d9ec795ce14876bf6be31c34a4f9d1d2cf20134
pploszczyca <piotrus.plo@gmail.com>
2024-05-31 19:40:46 +0200

    Added rest of the implementation of statement parser

--- parser/statement/statementParser.go
+++ parser/statement/statementParser.go
@@ -10,17 +10,20 @@ type statementParser struct {
 	expressionListParser parser.NodeParser
 	expressionParser     parser.NodeParser
 	relopParser          parser.NodeParser
+	varListParser        parser.NodeParser
 }
 
 func NewStatementParser(
 	expressionListParser parser.NodeParser,
 	expressionParser parser.NodeParser,
 	relopParser parser.NodeParser,
+	varListParser parser.NodeParser,
 ) parser.NodeParser {
 	return &statementParser{
 		expressionListParser: expressionListParser,
 		expressionParser:     expressionParser,
 		relopParser:          relopParser,
+		varListParser:        varListParser,
 	}
 }
 
@@ -50,7 +53,31 @@ func (s statementParser) Parse(iterator *domain.TokenIterator) (*domain.Node, er
 			return nil, err
 		}
 
-	// TODO: Implement parsing of other statements
+	case domain.Input:
+		err := s.parseInput(token, iterator, &statementNode)
+		if err != nil {
+			return nil, err
+		}
+
+	case domain.Let:
+		err := s.parseLet(token, iterator, &statementNode)
+		if err != nil {
+			return nil, err
+		}
+
+	case domain.Gosub:
+		err := s.parseGosub(token, iterator, &statementNode)
+		if err != nil {
+			return nil, err
+		}
+
+	case domain.Return:
+	case domain.Clear:
+	case domain.List:
+	case domain.Run:
+	case domain.End:
+		statementNode.AddChildToken(token)
+		iterator.Next()
 
 	default:
 		return nil, fmt.Errorf("unexpected statement: %v", token.Type)
@@ -120,6 +147,77 @@ func (s statementParser) parseIf(
 }
 
 func (s statementParser) parseGoto(
+	token domain.Token,
+	iterator *domain.TokenIterator,
+	statementNode *domain.Node,
+) error {
+	statementNode.AddChildToken(token)
+	iterator.Next()
+
+	expressionNode, err := s.expressionParser.Parse(iterator)
+	if err != nil {
+		return err
+	}
+	statementNode.AddChild(expressionNode)
+
+	return nil
+}
+
+func (s statementParser) parseInput(
+	token domain.Token,
+	iterator *domain.TokenIterator,
+	statementNode *domain.Node,
+) error {
+	statementNode.AddChildToken(token)
+	iterator.Next()
+
+	varListNode, err := s.varListParser.Parse(iterator)
+	if err != nil {
+		return err
+	}
+	statementNode.AddChild(varListNode)
+
+	return nil
+}
+
+func (s statementParser) parseLet(
+	token domain.Token,
+	iterator *domain.TokenIterator,
+	statementNode *domain.Node,
+) error {
+	statementNode.AddChildToken(token)
+	iterator.Next()
+
+	token, err := iterator.Current()
+	if err != nil {
+		return err
+	}
+	if token.Type != domain.Identifier {
+		return fmt.Errorf("expected identifier")
+	}
+	statementNode.AddChildToken(token)
+	iterator.Next()
+
+	token, err = iterator.Current()
+	if err != nil {
+		return err
+	}
+	if token.Type != domain.Equal {
+		return fmt.Errorf("expected equal")
+	}
+	statementNode.AddChildToken(token)
+	iterator.Next()
+
+	expressionNode, err := s.expressionParser.Parse(iterator)
+	if err != nil {
+		return err
+	}
+	statementNode.AddChild(expressionNode)
+
+	return nil
+}
+
+func (s statementParser) parseGosub(
 	token domain.Token,
 	iterator *domain.TokenIterator,
 	statementNode *domain.Node,
