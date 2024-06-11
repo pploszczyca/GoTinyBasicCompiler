d743be324e3731a0f0a912ae616598840e33e82e
pploszczyca <piotrus.plo@gmail.com>
2024-06-11 14:45:10 +0200

    Added extra test case

--- parser/line/lineParser_test.go
+++ parser/line/lineParser_test.go
@@ -42,6 +42,39 @@ func TestLineParser_Parse(t *testing.T) {
 		}
 	})
 
+	t.Run("parses line with number and statement and eof", func(t *testing.T) {
+		identifierNode := &domain.Node{Type: domain.IdentifierNode}
+		fakeStatementParser := &testutils.FakeNodeParser{
+			ParseMock: func(iterator *domain.TokenIterator) (*domain.Node, error) {
+				iterator.Next()
+				return identifierNode, nil
+			},
+		}
+		tokens := []domain.Token{
+			{Type: domain.Number},
+			{Type: domain.Identifier},
+			{Type: domain.Eof},
+		}
+		iterator := domain.NewTokenIterator(tokens)
+		expectedLineNode := &domain.Node{
+			Type: domain.LineNode,
+			Children: []*domain.Node{
+				{Type: domain.NumberNode, Token: tokens[0]},
+				identifierNode,
+			},
+		}
+
+		lp := NewLineParser(fakeStatementParser)
+		lineNode, err := lp.Parse(&iterator)
+
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		if !reflect.DeepEqual(lineNode, expectedLineNode) {
+			t.Errorf("Expected %v, got %v", expectedLineNode, lineNode)
+		}
+	})
+
 	t.Run("returns error when statement parser returns error", func(t *testing.T) {
 		expectedError := errors.New("parse error")
 		fakeStatementParser := &testutils.FakeNodeParser{
