eb5816f4d12337ca2bb5f58d7b46ce3560351022
pploszczyca <piotrus.plo@gmail.com>
2024-06-13 22:06:31 +0200

    Adjusted emitter tests to be parametrized

--- emiter/emitter_test.go
+++ emiter/emitter_test.go
@@ -7,73 +7,70 @@ import (
 )
 
 func TestCEmitter_Emit(t *testing.T) {
-	t.Run("returns C empty program when program tree is empty", func(t *testing.T) {
-		programTree := &domain.ProgramTree{}
-		expectedResult := `#include <stdio.h>
+	tests := []struct {
+		name           string
+		programTree    *domain.ProgramTree
+		expectedResult string
+		tokenEmitter   TokenEmitter
+	}{
+		{
+			name:        "returns C empty program when program tree is empty",
+			programTree: &domain.ProgramTree{},
+			expectedResult: `#include <stdio.h>
 int main() {
 }
-`
-		emitter := NewCEmitter(&testutils.FakeTokenEmitter{})
-		result, err := emitter.Emit(programTree)
-
-		if err != nil {
-			t.Errorf("Error: %s\n", err)
-		}
-		if result != expectedResult {
-			t.Errorf("Expected: %s, but got: %s\n", expectedResult, result)
-		}
-	})
-
-	t.Run("returns C program with end token", func(t *testing.T) {
-		endToken := domain.Token{Type: domain.End}
-		lineNode := &domain.Node{
-			Type: domain.LineNode,
-			Children: []*domain.Node{
-				{Type: domain.NumberNode, Token: domain.Token{Type: domain.Number, Value: "10"}},
-				{Type: domain.StatementNode, Children: []*domain.Node{{Token: endToken}}},
+`,
+			tokenEmitter: &testutils.FakeTokenEmitter{},
+		},
+		{
+			name: "returns C program with end token",
+			programTree: &domain.ProgramTree{
+				Nodes: []*domain.Node{
+					{
+						Type: domain.LineNode,
+						Children: []*domain.Node{
+							{Type: domain.NumberNode, Token: domain.Token{Type: domain.Number, Value: "10"}},
+							{Type: domain.StatementNode, Children: []*domain.Node{{Token: domain.Token{Type: domain.End}}}},
+						},
+					},
+				},
 			},
-		}
-		programTree := &domain.ProgramTree{Nodes: []*domain.Node{lineNode}}
-		expectedResult := `#include <stdio.h>
+			expectedResult: `#include <stdio.h>
 int main() {
     label_10:
     return 0;
 }
-`
-
-		emitter := NewCEmitter(NewCTokenEmitter())
-		result, err := emitter.Emit(programTree)
-
-		if err != nil {
-			t.Errorf("Error: %s\n", err)
-		}
-		if result != expectedResult {
-			t.Errorf("Expected: \n%s, but got: \n%s\n", expectedResult, result)
-		}
-	})
-
-	t.Run("returns C program with print node", func(t *testing.T) {
-		lineNode := &domain.Node{
-			Type: domain.LineNode,
-			Children: []*domain.Node{
-				{Type: domain.NumberNode, Token: domain.Token{Type: domain.Number, Value: "10"}},
-				{
-					Type: domain.StatementNode,
-					Children: []*domain.Node{
-						{Token: domain.Token{Type: domain.Print}},
-						{
-							Type: domain.ExpressionListNode,
-							Children: []*domain.Node{
-								{Token: domain.Token{Type: domain.String, Value: "\"Hello\""}},
-								{Token: domain.Token{Type: domain.Comma}},
-								{Token: domain.Token{Type: domain.Identifier, Value: "A"}},
-								{Token: domain.Token{Type: domain.Comma}},
-								{
-									Type: domain.ExpressionNode,
-									Children: []*domain.Node{
-										{Token: domain.Token{Type: domain.Number, Value: "1"}},
-										{Token: domain.Token{Type: domain.Plus}},
-										{Token: domain.Token{Type: domain.Number, Value: "2"}},
+`,
+			tokenEmitter: NewCTokenEmitter(),
+		},
+		{
+			name: "returns C program with print node",
+			programTree: &domain.ProgramTree{
+				Nodes: []*domain.Node{
+					{
+						Type: domain.LineNode,
+						Children: []*domain.Node{
+							{Type: domain.NumberNode, Token: domain.Token{Type: domain.Number, Value: "10"}},
+							{
+								Type: domain.StatementNode,
+								Children: []*domain.Node{
+									{Token: domain.Token{Type: domain.Print}},
+									{
+										Type: domain.ExpressionListNode,
+										Children: []*domain.Node{
+											{Token: domain.Token{Type: domain.String, Value: "\"Hello\""}},
+											{Token: domain.Token{Type: domain.Comma}},
+											{Token: domain.Token{Type: domain.Identifier, Value: "A"}},
+											{Token: domain.Token{Type: domain.Comma}},
+											{
+												Type: domain.ExpressionNode,
+												Children: []*domain.Node{
+													{Token: domain.Token{Type: domain.Number, Value: "1"}},
+													{Token: domain.Token{Type: domain.Plus}},
+													{Token: domain.Token{Type: domain.Number, Value: "2"}},
+												},
+											},
+										},
 									},
 								},
 							},
@@ -81,23 +78,27 @@ int main() {
 					},
 				},
 			},
-		}
-		programTree := &domain.ProgramTree{Nodes: []*domain.Node{lineNode}}
-		expectedResult := `#include <stdio.h>
+			expectedResult: `#include <stdio.h>
 int main() {
     label_10:
     printf("%s,%d,%d", "Hello", A, 1+2);
 }
-`
+`,
+			tokenEmitter: NewCTokenEmitter(),
+		},
+	}
 
-		emitter := NewCEmitter(NewCTokenEmitter())
-		result, err := emitter.Emit(programTree)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			emitter := NewCEmitter(tt.tokenEmitter)
+			result, err := emitter.Emit(tt.programTree)
 
-		if err != nil {
-			t.Errorf("Error: %s\n", err)
-		}
-		if result != expectedResult {
-			t.Errorf("Expected: \n%s, but got: \n%s\n", expectedResult, result)
-		}
-	})
+			if err != nil {
+				t.Errorf("Error: %s\n", err)
+			}
+			if result != tt.expectedResult {
+				t.Errorf("Expected: \n%s, but got: \n%s\n", tt.expectedResult, result)
+			}
+		})
+	}
 }
